9f5d4b53a01646e04a8cafffe44f4d61a5020506
MalekMohanna <malekmhana@gmail.com>
2022-12-07 15:45:25 +0200

    first commit

---
+++ firstproject/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo</h1>
+      <p>To do list:</p>
+      <ul>
+        <li>hi</li>
+        <li>hello</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
