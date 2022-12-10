0bd080da4cca0286a0dc00c7e482e2dad3c12b78
MalekMohanna <malekmhana@gmail.com>
2022-12-10 13:33:18 +0200

    updated

--- first-jsx/src/components/PersonCard.jsx
+++ first-jsx/src/components/PersonCard.jsx
@@ -1,12 +1,24 @@
 import React, { Component } from 'react'
 
 export default class PersonCard extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      newAge : props.age,
+    }
+  }
+
+  increase =()=>{
+    this.setState({newAge:(this.state.newAge)+1});
+  }
+
   render() {
     return (
       <div>
         <h1> {this.props.firstName}, {this.props.lastName}</h1>
-        <h2>Age : {this.props.age} </h2>
+        <h2>Age : {this.state.newAge} </h2>
         <h2>Hair color : {this.props.color} </h2>
+        <button onClick={this.increase} >Increase age</button>
       </div>
     )
   }
